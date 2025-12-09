3f4848fd138a97f414f8f788b026406eaf4144e2
Jason <jason891588@gmail.com>
2025-12-09 15:43:19 +0800

    [feat] Initialize Quicktix project with Maven setup and basic Spring Boot application structure

---
+++ src/test/java/com/jason/quicktix/QuicktixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.quicktix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicktixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
